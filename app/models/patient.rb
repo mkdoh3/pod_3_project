c9214dfe4173c9faf6121484c19fdead68ea1c62
Andrew Alteri <andrewsalteri@gmail.com>
2020-08-26 10:23:32 -0500

    WIP: Adding CRUD functionality for resources.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Patient < ApplicationRecord
+    validates :name, :phone_number, :age, :address, presence: true
+    validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/}
 end
