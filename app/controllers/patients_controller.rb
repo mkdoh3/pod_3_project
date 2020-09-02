883060f4e1130dc12806273223541fe7a66a9850
mkdoh3 <mkdoherty3@gmail.com>
2020-09-02 11:55:00 -0500

    branching demo

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,34 +1,34 @@
 # frozen_string_literal: true
 
 class PatientsController < ApplicationController
-  before_action :set_patient, only: [:show, :edit, :update, :destroy]
-<<<<<<< HEAD
-=======
-
->>>>>>> ce66236948d03ce022eb44d3a5b473abb28d6471
-
+  before_action :set_patient, only: %i[show edit update destroy]
+  #
+  #
+  #
+  # testing
+  #
+  #
+  #
   def index
     @patients = Patient.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @patient = Patient.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
-    @patient = Patient.new(patient_params(:name, :phone_number, :age, :address,))
+    @patient = Patient.new(patient_params(:name, :phone_number, :age, :address))
     if @patient.save
       redirect_to @patient
 
     else
 
-      render "new"
+      render 'new'
     end
   end
 
@@ -36,7 +36,7 @@ class PatientsController < ApplicationController
     if @patient.update(patient_params(:name, :phone_number, :age, :address))
       redirect_to @patient
     else
-      render "edit"
+      render 'edit'
     end
   end
 
@@ -45,7 +45,7 @@ class PatientsController < ApplicationController
     redirect_to patients_url
   end
 
-private
+  private
 
   def set_patient
     @patient = Patient.find(params[:id])
@@ -54,6 +54,4 @@ private
   def patient_params(*args)
     params.require(:patient).permit(*args)
   end
-
-
 end
